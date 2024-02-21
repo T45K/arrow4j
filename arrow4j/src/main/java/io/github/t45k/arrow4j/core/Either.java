89f9e053297ee2ee77ccb9a9756eec72da613c15
T45K <tasktas9@gmail.com>
2024-02-21 23:41:30 +0900

    add test

--- arrow4j/src/main/java/io/github/t45k/arrow4j/core/Either.java
+++ arrow4j/src/main/java/io/github/t45k/arrow4j/core/Either.java
@@ -1,13 +1,5 @@
 package io.github.t45k.arrow4j.core;
 
-import io.github.t45k.arrow4j.util.Function10;
-import io.github.t45k.arrow4j.util.Function3;
-import io.github.t45k.arrow4j.util.Function4;
-import io.github.t45k.arrow4j.util.Function5;
-import io.github.t45k.arrow4j.util.Function6;
-import io.github.t45k.arrow4j.util.Function7;
-import io.github.t45k.arrow4j.util.Function8;
-import io.github.t45k.arrow4j.util.Function9;
 import io.github.t45k.arrow4j.util.Pair;
 import jakarta.annotation.Nullable;
 
@@ -19,24 +11,24 @@ import java.util.function.Predicate;
 import static io.github.t45k.arrow4j.core.Option.None;
 import static io.github.t45k.arrow4j.core.Option.Some;
 
-@SuppressWarnings({"unused", "unchecked", "rawtypes"})
+@SuppressWarnings({"unused", "unchecked"})
 public sealed interface Either<A, B> {
 
-    record Left<A>(A value) implements Either {
+    // Unfortunately, Java doesn't have bottom type (e.g., Nothing in Kotlin).
+    // Therefore, we need to let Left and Right types have both A and B.
+    record Left<A, B>(A value) implements Either<A, B> {
     }
 
     static <A, B> Either<A, B> left(final A value) {
-        return new Left<>(value);
+        return (new Left<>(value));
     }
 
-    record Right<B>(B value) implements Either {
+    record Right<A, B>(B value) implements Either<A, B> {
     }
 
     static <A, B> Either<A, B> right(final B value) {
         return new Right<>(value);
     }
-
-    Right<Void> voidRight = new Right<>(null);
 
     static <A, B> Either<A, B> flatten(final Either<A, Either<A, B>> either) {
         return either.flatMap(Function.identity());
@@ -59,186 +51,29 @@ public sealed interface Either<A, B> {
 
     // bisequence, bisequenceOption, bisequenceNullable are omitted
 
-    static <E, A, B, Z> Either<A, Z> zipOrAccumulate(
-        final BiFunction<E, E, E> combine,
-        final Either<E, A> a,
-        final Either<E, B> b,
-        final BiFunction<A, B, Z> transform
-    ) {
-        return zipOrAccumulate(combine, a, b, voidRight, (aa, bb, unused) -> transform.apply(aa, bb)
-        );
-    }
-
-    static <E, A, B, C, Z> Either<E, Z> zipOrAccumulate(
-        final BiFunction<E, E, E> combine,
-        final Either<E, A> a,
-        final Either<E, B> b,
-        final Either<E, C> c,
-        final Function3<A, B, C, Z> transform
-    ) {
-        return zipOrAccumulate(combine, a, b, c, voidRight, (aa, bb, cc, unused) -> transform.apply(aa, bb, cc)
-        );
-    }
-
-    static <E, A, B, C, D, Z> Either<E, Z> zipOrAccumulate(
-        final BiFunction<E, E, E> combine,
-        final Either<E, A> a,
-        final Either<E, B> b,
-        final Either<E, C> c,
-        final Either<E, D> d,
-        final Function4<A, B, C, D, Z> transform
-    ) {
-        return zipOrAccumulate(combine, a, b, c, d, voidRight, (aa, bb, cc, dd, unused) -> transform.apply(aa, bb, cc, dd)
-        );
-    }
-
-    static <E, A, B, C, D, EE, Z> Either<E, Z> zipOrAccumulate(
-        final BiFunction<E, E, E> combine,
-        final Either<E, A> a,
-        final Either<E, B> b,
-        final Either<E, C> c,
-        final Either<E, D> d,
-        final Either<E, EE> e,
-        final Function5<A, B, C, D, EE, Z> transform
-    ) {
-        return zipOrAccumulate(combine, a, b, c, d, e, voidRight,
-            (aa, bb, cc, dd, ee, unused) -> transform.apply(aa, bb, cc, dd, ee)
-        );
-    }
-
-    static <E, A, B, C, D, EE, F, Z> Either<E, Z> zipOrAccumulate(
-        final BiFunction<E, E, E> combine,
-        final Either<E, A> a,
-        final Either<E, B> b,
-        final Either<E, C> c,
-        final Either<E, D> d,
-        final Either<E, EE> e,
-        final Either<E, F> f,
-        final Function6<A, B, C, D, EE, F, Z> transform
-    ) {
-        return zipOrAccumulate(combine, a, b, c, d, e, f, voidRight,
-            (aa, bb, cc, dd, ee, ff, unused) -> transform.apply(aa, bb, cc, dd, ee, ff)
-        );
-    }
-
-    static <E, A, B, C, D, EE, F, G, Z> Either<E, Z> zipOrAccumulate(
-        final BiFunction<E, E, E> combine,
-        final Either<E, A> a,
-        final Either<E, B> b,
-        final Either<E, C> c,
-        final Either<E, D> d,
-        final Either<E, EE> e,
-        final Either<E, F> f,
-        final Either<E, G> g,
-        final Function7<A, B, C, D, EE, F, G, Z> transform
-    ) {
-        return zipOrAccumulate(combine, a, b, c, d, e, f, g, voidRight,
-            (aa, bb, cc, dd, ee, ff, gg, unused) -> transform.apply(aa, bb, cc, dd, ee, ff, gg)
-        );
-    }
-
-    static <E, A, B, C, D, EE, F, G, H, Z> Either<E, Z> zipOrAccumulate(
-        final BiFunction<E, E, E> combine,
-        final Either<E, A> a,
-        final Either<E, B> b,
-        final Either<E, C> c,
-        final Either<E, D> d,
-        final Either<E, EE> e,
-        final Either<E, F> f,
-        final Either<E, G> g,
-        final Either<E, H> h,
-        final Function8<A, B, C, D, EE, F, G, H, Z> transform
-    ) {
-        return zipOrAccumulate(combine, a, b, c, d, e, f, g, h, voidRight,
-            (aa, bb, cc, dd, ee, ff, gg, hh, unused) -> transform.apply(aa, bb, cc, dd, ee, ff, gg, hh)
-        );
-    }
-
-    static <E, A, B, C, D, EE, F, G, H, I, Z> Either<E, Z> zipOrAccumulate(
-        final BiFunction<E, E, E> combine,
-        final Either<E, A> a,
-        final Either<E, B> b,
-        final Either<E, C> c,
-        final Either<E, D> d,
-        final Either<E, EE> e,
-        final Either<E, F> f,
-        final Either<E, G> g,
-        final Either<E, H> h,
-        final Either<E, I> i,
-        final Function9<A, B, C, D, EE, F, G, H, I, Z> transform
-    ) {
-        return zipOrAccumulate(combine, a, b, c, d, e, f, g, h, i, voidRight,
-            (aa, bb, cc, dd, ee, ff, gg, hh, ii, unused) -> transform.apply(aa, bb, cc, dd, ee, ff, gg, hh, ii)
-        );
-    }
-
-    @SuppressWarnings("DuplicatedCode")
-    static <E, A, B, C, D, EE, F, G, H, I, J, Z> Either<E, Z> zipOrAccumulate(
-        final BiFunction<E, E, E> combine,
-        final Either<E, A> a,
-        final Either<E, B> b,
-        final Either<E, C> c,
-        final Either<E, D> d,
-        final Either<E, EE> e,
-        final Either<E, F> f,
-        final Either<E, G> g,
-        final Either<E, H> h,
-        final Either<E, I> i,
-        final Either<E, J> j,
-        final Function10<A, B, C, D, EE, F, G, H, I, J, Z> transform
-    ) {
-        if (a instanceof Right<A>(var valueA) &&
-            b instanceof Right<B>(var valueB) &&
-            c instanceof Right<C>(var valueC) &&
-            d instanceof Right<D>(var valueD) &&
-            e instanceof Right<EE>(var valueE) &&
-            f instanceof Right<F>(var valueF) &&
-            g instanceof Right<G>(var valueG) &&
-            h instanceof Right<H>(var valueH) &&
-            i instanceof Right<I>(var valueI) &&
-            j instanceof Right<J>(var valueJ)
-        ) {
-            return new Right<>(transform.apply(valueA, valueB, valueC, valueD, valueE, valueF, valueG, valueH, valueI, valueJ));
-        }
-
-        E accumulatedError = null;
-        final BiFunction<E, E, E> combineIfNotNull = (e1, e2) -> e1 == null ? e2 : combine.apply(e1, e2);
-        if (a instanceof Left<E>(var error)) accumulatedError = combineIfNotNull.apply(accumulatedError, error);
-        if (b instanceof Left<E>(var error)) accumulatedError = combineIfNotNull.apply(accumulatedError, error);
-        if (c instanceof Left<E>(var error)) accumulatedError = combineIfNotNull.apply(accumulatedError, error);
-        if (d instanceof Left<E>(var error)) accumulatedError = combineIfNotNull.apply(accumulatedError, error);
-        if (e instanceof Left<E>(var error)) accumulatedError = combineIfNotNull.apply(accumulatedError, error);
-        if (f instanceof Left<E>(var error)) accumulatedError = combineIfNotNull.apply(accumulatedError, error);
-        if (g instanceof Left<E>(var error)) accumulatedError = combineIfNotNull.apply(accumulatedError, error);
-        if (h instanceof Left<E>(var error)) accumulatedError = combineIfNotNull.apply(accumulatedError, error);
-        if (i instanceof Left<E>(var error)) accumulatedError = combineIfNotNull.apply(accumulatedError, error);
-        if (j instanceof Left<E>(var error)) accumulatedError = combineIfNotNull.apply(accumulatedError, error);
-
-        return new Left(accumulatedError);
-    }
-
+    // TODO: zipOrAccumulate
     // TODO: zipOrAccumulateNonEmptyList will be implemented after implementing NonEmptyList
 
     default boolean isLeft() {
-        return this instanceof Left<A>;
+        return this instanceof Left;
     }
 
     default boolean isRight() {
-        return this instanceof Right<A>;
+        return this instanceof Right;
     }
 
     default boolean isLeft(final Predicate<A> predicate) {
-        return this instanceof Left<A> left && predicate.test(left.value);
+        return this instanceof Left<A, B> left && predicate.test(left.value);
     }
 
     default boolean isRight(final Predicate<B> predicate) {
-        return this instanceof Right<B> right && predicate.test(right.value);
+        return this instanceof Right<A, B> right && predicate.test(right.value);
     }
 
     default <C> C fold(final Function<A, C> ifLeft, final Function<B, C> ifRight) {
         return switch (this) {
-            case Right<B>(var value) -> ifRight.apply(value);
-            case Left<A>(var value) -> ifLeft.apply(value);
+            case Right<A, B>(var value) -> ifRight.apply(value);
+            case Left<A, B>(var value) -> ifLeft.apply(value);
         };
     }
 
@@ -248,8 +83,8 @@ public sealed interface Either<A, B> {
 
     default <C> Either<A, C> map(final Function<B, C> f) {
         return switch (this) {
-            case Right<B>(var value) -> new Right<>(f.apply(value));
-            case Left<B> left -> (Either<A, C>) left;
+            case Right<A, B>(var value) -> new Right<>(f.apply(value));
+            case Left<A, B> left -> (Either<A, C>) left;
         };
     }
 
@@ -258,14 +93,14 @@ public sealed interface Either<A, B> {
     }
 
     default Either<A, B> onRight(final Consumer<B> action) {
-        if (this instanceof Right<B>(var value)) {
+        if (this instanceof Right<A, B>(var value)) {
             action.accept(value);
         }
         return this;
     }
 
     default Either<A, B> onLeft(final Consumer<A> action) {
-        if (this instanceof Left<A>(var value)) {
+        if (this instanceof Left<A, B>(var value)) {
             action.accept(value);
         }
         return this;
@@ -289,8 +124,8 @@ public sealed interface Either<A, B> {
 
     default <C> Either<A, C> flatMap(final Function<B, Either<A, C>> f) {
         return switch (this) {
-            case Right<B>(var value) -> f.apply(value);
-            case Left<A> left -> (Either<A, C>) left;
+            case Right<A, B>(var value) -> f.apply(value);
+            case Left<A, B> left -> (Either<A, C>) left;
         };
     }
 
@@ -300,12 +135,12 @@ public sealed interface Either<A, B> {
 
     default Either<A, B> combine(final Either<A, B> other, final BiFunction<A, A, A> combineLeft, final BiFunction<B, B, B> combineRight) {
         return switch (new Pair<>(this, other)) {
-            case Pair(Left<A> thisLeft, Left<A> otherLeft) ->
-                Either.left(combineLeft.apply(thisLeft.value, otherLeft.value));
-            case Pair(Right<B> thisRight, Right<B> otherRight) ->
-                Either.right(combineRight.apply(thisRight.value, otherRight.value));
-            case Pair(Right<B> thisRight, Left<A> otherLeft) -> otherLeft;
-            case Pair(Left<A> thisLeft, Right<B> otherRight) -> thisLeft;
+            case Pair(Left<A, B>(var thisLeft), Left<A, B>(var otherLeft)) ->
+                Either.left(combineLeft.apply(thisLeft, otherLeft));
+            case Pair(Right<A, B>(var thisRight), Right<A, B>(var otherRight)) ->
+                Either.right(combineRight.apply(thisRight, otherRight));
+            case Pair(Right<A, B> unused, Left<A, B> otherLeft) -> otherLeft;
+            case Pair(Left<A, B> thisLeft, Right<A, B> unused) -> thisLeft;
         };
     }
 }
